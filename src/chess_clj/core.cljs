8ce141ce365a89279906390f6745405ee4500dbb
shidel.dev <joeshidel@gmail.com>
2014-10-31 13:07:10 -0500

    adds move logic for night and pawn, also adds core.async

--- src/chess_clj/core.cljs
+++ src/chess_clj/core.cljs
@@ -1,34 +1,139 @@
 (ns chess-clj.core
+  (:require-macros  [cljs.core.async.macros :refer  [go]])
   (:require
-    [dommy.core :as dom :refer-macros [sel sel1]]))
+    [dommy.core :as dom :refer-macros [sel sel1]]
+    [cljs.core.async :refer  [<! put! chan]]))
 
 (enable-console-print!)
 
+;;values -------------------------------------------------------------
+
 (def board-state
-  (atom [[{:piece :rook :color :black} {:piece :knight :color :black} {:piece :bishop :color :black} {:piece :king :color :black} {:piece :queen :color :black}  {:color :black :piece :bishop}  {:color :black :piece :knight}  {:color :black :piece :rook}]
-            [{:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black}{:piece :pawn :first-move true :color :black}{:piece :pawn :first-move true :color :black}]
-            [{} {} {} {} {} {} {} {}]
-            [{} {} {} {} {} {} {} {}]
-            [{} {} {} {} {} {} {} {}]
-            [{} {} {} {} {} {} {} {}]
+  (atom [[{:piece :rook :color :white} {:piece :knight :color :white} {:piece :bishop :color :white} {:piece :king :color :white} {:piece :queen :color :white}  {:color :white :piece :bishop}  {:color :white :piece :knight}  {:color :white :piece :rook}]
             [{:piece :pawn :first-move true :color :white} {:piece :pawn :first-move true :color :white} {:piece :pawn :first-move true :color :white} {:piece :pawn :first-move true :color :white} {:piece :pawn :first-move true :color :white} {:piece :pawn :first-move true :color :white}{:piece :pawn :first-move true :color :white}{:piece :pawn :first-move true :color :white}]
-            [{:piece :rook :color :white} {:piece :knight :color :white} {:piece :bishop :color :white} {:piece :king :color :white} {:piece :queen :color :white}  {:color :white :piece :bishop}  {:color :white :piece :knight}  {:color :white :piece :rook}]]))
+            [{} {} {} {} {} {} {} {}]
+            [{} {} {} {} {} {} {} {}]
+            [{} {} {} {} {} {} {} {}]
+            [{} {} {} {} {} {} {} {}]
+            [{:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black} {:piece :pawn :first-move true :color :black}{:piece :pawn :first-move true :color :black}{:piece :pawn :first-move true :color :black}]
+            [{:piece :rook :color :black} {:piece :knight :color :black} {:piece :bishop :color :black} {:piece :king :color :black} {:piece :queen :color :black}  {:color :black :piece :bishop}  {:color :black :piece :knight}  {:color :black :piece :rook}]]))
 
 (def piece-codes { :rook { :black "&#9820;" :white "&#9814;" } :knight { :black "&#9822;" :white "&#9816;"} :bishop { :black "&#9821;" :white "&#9815;"} :king { :black "&#9819;" :white "&#9813;" } :queen { :black "&#9818;" :white "&#9812;" } :pawn { :white "&#9817;" :black "&#9823;" }})
 
+(def current-turn (atom :white))
+
+(def selected-piece (atom false))
+
+;;helper functions --------------------------------------------------
+
+;; creates an html string for a chess-piece
 (defn chess-piece [piece color]
   (str "<a href='#' class='" (name piece) " " (name color) "'>" (get-in piece-codes [piece color]) "</a>"))
 
+(defn highlight [cords]
+  (let [highlighted-elems (dom/sel :.highlight)]
+    (if highlighted-elems
+      (doseq [elem highlighted-elems] (dom/remove-class! elem "highlight"))))
+  (doseq [cord cords]
+    (let [dom-cord (rel-cords cord)]
+      (dom/add-class! (js/document.getElementById (apply str (reverse dom-cord))) "highlight"))))
+
+(defn rotate-board [] (doall (apply map list @board-state)))
+
+(defn change-turns [] (swap! board-state #(map reverse (reverse %))))
+
+(defn rel-cords [cords]
+  (if (= @current-turn :black) cords
+    (let [[x y] cords] [(js/Math.abs (- x 7)), (js/Math.abs (- y 7))])))
+
+(defn not-nil-or-empty [board cords]
+  (and (not= nil (get-in board cords)) (empty? (get-in board cords))))
+
+
+;; click stream ---------------------------------------------------
+
+;; sends the cell cords through chan on a click
+(defn piece-click-handler [cell c]
+  (let [data-cell (rel-cords cell)] (put! c data-cell)))
+
+;; create channel which clicks on board cells will be sent through
+(defn create-click-chan []
+ (let [c (chan)]
+   (doseq [x [0 1 2 3 4 5 6 7]
+           y [0 1 2 3 4 5 6 7]]
+     (dom/listen! (js/document.getElementById (str x y)) :click
+       (fn [e]
+         (piece-click-handler [y x] c))))
+   c))
+
+;; takes clicks off of channel
+(defn click-stream-processor [c]
+  (go
+    (while true
+      (make-or-present-move (<! c)))))
+
+;; game logic ------------------------------------------------------
+
+(defn take-while-valid [cords]
+   (take-while (fn [cord] (not-nil-or-empty @board-state cord)) cords))
+
+(defn pawn-moves [cell cords]
+  (take-while-valid
+    (let [[y x] cords]
+      (if (:first-move cell) [[(inc y) x] [(+ 2 y) x]]
+        [(inc y) x]))))
+
+(defn knight-moves [cell cords]
+    (filter (fn [cord] (not-nil-or-empty @board-state cord))
+      (doall
+        (for [x [-1 -2 1 2]
+              y [-1 -2 1 2]
+            :when (not= (js/Math.abs x) (js/Math.abs y))]
+        [(+ y (first cords)) (+ x (second cords))]))))
+
+(def move-fn
+  {:pawn pawn-moves :knight knight-moves})
+
+(defn make-move [cell cords]
+  (swap! board-state (fn [board]
+    (-> board
+        (assoc-in cords (dissoc @selected-piece :position))
+        (assoc-in (:position @selected-piece {})))))
+  (swap! selected-piece (fn [p] false)))
+
+(defn make-or-reject-move [cell cords]
+  (println [cell cords])
+  (if (some #{cords} (get-moves @selected-piece (:position @selected-piece)))
+    (make-move cell cords))
+  (render-board-state))
+
+(defn get-moves [cell cords]
+  ((move-fn (:piece cell)) cell cords))
+
+(defn highlight-moves [cell cords]
+  (swap! selected-piece #(assoc (get-in @board-state cords) :position cords))
+  (highlight (get-moves cell cords)))
+
+(defn make-or-present-move [cords]
+  (let [cell (get-in @board-state cords)]
+    (cond
+      (and (empty? cell) (not= false @selected-piece)) (make-or-reject-move cell cords)
+      (and (not (empty? cell)) (= @current-turn (:color cell))) (highlight-moves cell cords))))
+
+;; display logic --------------------------------------------------
+
 (defn render-board-state []
-  (doall (for [x [0 1 2 3 4 5 6 7]
-               y [0 1 2 3 4 5 6 7]]
+  (let [board (vec (map #(-> % reverse vec) (-> @board-state reverse vec)))]
+    (doseq [x [0 1 2 3 4 5 6 7]
+          y [0 1 2 3 4 5 6 7]]
     (let [dom-cell (js/document.getElementById (str y x))
-          data-cell (get-in @board-state [x y])
+          data-cell (get-in board [x y])
           piece-html (if (empty? data-cell) "" (chess-piece (:piece data-cell) (:color data-cell)))]
-      (println data-cell)
-      (dom/set-html! dom-cell piece-html)))))
+      (dom/set-html! dom-cell piece-html))))
 
-(chess-piece :bishop :black)
+  (let [highlighted-elems (dom/sel :.highlight)]
+    (if highlighted-elems
+      (doseq [elem highlighted-elems] (dom/remove-class! elem "highlight")))))
 
-
-(js/document.body.addEventListener "load" render-board-state)
+(render-board-state)
+(click-stream-processor (create-click-chan))
