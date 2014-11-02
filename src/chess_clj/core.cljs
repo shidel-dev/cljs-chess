1f985fb2c0b5cd13f39ddffb8687be6b75f7cd15
shidel.dev <joeshidel@gmail.com>
2014-11-02 10:19:28 -0600

    removes unused functions

--- src/chess_clj/core.cljs
+++ src/chess_clj/core.cljs
@@ -3,12 +3,6 @@
     [chess-clj.view :as view :refer [render-board-state]]
     [chess-clj.game-logic :as logic :refer [create-click-stream]]))
 
-;;helper functions --------------------------------------------------
-
-(defn rotate-board [] (doall (apply map list @board-state)))
-
-(defn change-turns [] (swap! board-state #(map reverse (reverse %))))
-
 (view/render-board-state)
 
 (logic/create-click-stream)
